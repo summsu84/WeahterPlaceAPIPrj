7e9f647731f383237d264ba78c5ed74fee0773e0
summsu84 <summsu84@gmail.com>
2019-07-16 13:05:13 +0900

    중기장소 조회

--- module/db/model/placeCodeSelectModel.js
+++ module/db/model/placeCodeSelectModel.js
@@ -25,6 +25,98 @@ class PlaceCodeSelectModel {
             }
         })
     }
+
+    /**
+     *  중기전망 지역코드 조회
+     * @param params
+     * @param callback
+     */
+    selectMiddleForecastPlaceCode(params, callback) {
+        const self = this;
+
+        const sql = 'SELECT A.* FROM tbl_weather_place_detail AS A, (select id from tbl_weather_place_district_code where  code = ?) AS B where place_distict_code_id = B.id';
+
+        const query = self.connection.query(sql, params,  function (err, rows){
+
+            if (err)
+                console.error(err);
+            else {
+                console.log(rows);
+                if (callback)
+                    callback(err, rows);
+            }
+        })
+    }
+
+
+    selectMiddleForecastAndTempAndLandPlaceCode(params, callback) {
+        const self = this;
+
+        const sql = 'select a.*, c.middle_forecast_place_code, d.middle_forecast_land_place_code, e.middle_forecast_temp_place_code from tbl_weather_place_city_level2_code as a, ' +
+            '(select place_city_level3_id from tbl_weather_place_district_code where code = ?) b\n' +
+            ',(select id, middle_forecast_place_code from tbl_weather_place_middle_forecast_code) as c, ' +
+            '(select id, middle_forecast_land_place_code from tbl_weather_place_middle_forecast_land_code) as d,\n' +
+            '(select id, middle_forecast_temp_place_code from tbl_weather_place_middle_forecast_temp_code) as e\n' +
+            'where a.id = b.place_city_level3_id and a.middle_forecast_land_place_code = c.id and a.middle_forecast_temp_place_code = e.id and a.middle_forecst_place_code = d.id';
+
+            'SELECT A.* FROM tbl_weather_place_detail AS A, (select id from tbl_weather_place_district_code where  code = ?) AS B where place_distict_code_id = B.id';
+
+        const query = self.connection.query(sql, params,  function (err, rows){
+
+            if (err)
+                console.error(err);
+            else {
+                console.log(rows);
+                if (callback)
+                    callback(err, rows);
+            }
+        })
+    }
+
+
+    /**
+     *  중기기온 지역코드 조회
+     * @param params
+     * @param callback
+     */
+    selectMiddleTempPlaceCode(params, callback) {
+        const self = this;
+
+        const sql = 'SELECT A.* FROM tbl_weather_place_detail AS A, (select id from tbl_weather_place_district_code where  code = ?) AS B where place_distict_code_id = B.id';
+
+        const query = self.connection.query(sql, params,  function (err, rows){
+
+            if (err)
+                console.error(err);
+            else {
+                console.log(rows);
+                if (callback)
+                    callback(err, rows);
+            }
+        })
+    }
+
+    /**
+     *  중기육상 지역코드 조회
+     * @param params
+     * @param callback
+     */
+    selectMiddleLandPlaceCode(params, callback) {
+        const self = this;
+
+        const sql = 'SELECT A.* FROM tbl_weather_place_detail AS A, (select id from tbl_weather_place_district_code where  code = ?) AS B where place_distict_code_id = B.id';
+
+        const query = self.connection.query(sql, params,  function (err, rows){
+
+            if (err)
+                console.error(err);
+            else {
+                console.log(rows);
+                if (callback)
+                    callback(err, rows);
+            }
+        })
+    }
 }
 
 module["exports"] = new PlaceCodeSelectModel();
--- route/index.js
+++ route/index.js
@@ -31,4 +31,89 @@ module.exports = (server) => {
         });
     });
 
+
+    /**
+     *  중기기상 전망, 기온, 육상 정보 지역 코드 조회
+     */
+    server.get('/place/middle/:place_code', (req, res, next) => {
+        const placeCodeSelectModel = require("../module/db/model/placeCodeSelectModel")
+        const params = [];
+        params.push(req.params.place_code)
+
+        placeCodeSelectModel.selectMiddleForecastAndTempAndLandPlaceCode(params, function(err, docs) {
+            if (err) {
+                console.error(err);
+                return next(
+                    new errors.InvalidContentError(err.errors.name.message),
+                );
+            }
+
+            res.send(docs[0]);
+            next();
+        });
+    });
+
+
+    /**
+     *  중기기상전망 코드 조회
+     */
+    server.get('/place/middle/forecast/:place_code', (req, res, next) => {
+        const placeCodeSelectModel = require("../module/db/model/placeCodeSelectModel")
+        const params = [];
+        params.push(req.params.place_code)
+
+        placeCodeSelectModel.selectMiddleForecastPlaceCode(params, function(err, docs) {
+            if (err) {
+                console.error(err);
+                return next(
+                    new errors.InvalidContentError(err.errors.name.message),
+                );
+            }
+
+            res.send(docs[0]);
+            next();
+        });
+    });
+
+    /**
+     *  중기기온 지역 코드 조회
+     */
+    server.get('/place/middle/temp/:place_code', (req, res, next) => {
+        const placeCodeSelectModel = require("../module/db/model/placeCodeSelectModel")
+        const params = [];
+        params.push(req.params.place_code)
+
+        placeCodeSelectModel.selectMiddleTempPlaceCode(params, function(err, docs) {
+            if (err) {
+                console.error(err);
+                return next(
+                    new errors.InvalidContentError(err.errors.name.message),
+                );
+            }
+
+            res.send(docs[0]);
+            next();
+        });
+    });
+
+    /**
+     *  중기육상예보 지역 코드 조회
+     */
+    server.get('/place/middle/land/:place_code', (req, res, next) => {
+        const placeCodeSelectModel = require("../module/db/model/placeCodeSelectModel")
+        const params = [];
+        params.push(req.params.place_code)
+
+        placeCodeSelectModel.selectMiddleTempPlaceCode(params, function(err, docs) {
+            if (err) {
+                console.error(err);
+                return next(
+                    new errors.InvalidContentError(err.errors.name.message),
+                );
+            }
+
+            res.send(docs[0]);
+            next();
+        });
+    });
 };
